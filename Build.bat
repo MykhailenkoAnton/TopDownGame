3f165ec380e68abba316063d81aee6bbda43f7c9
anton.mykhailenko <anton.mykhailenko@DPR-WKS-059>
2025-02-20 23:19:15 +0200

    2

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"C:\JenkinsJob\UE5Source\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=C:\JenkinsJob\UE5Projects\TopDown\TopDown.uproject ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory=C:\JenkinsJob\UE5Projects\TopDown\BuildFromBat ^
+-cook -stage -archive -pak -package -allmaps
